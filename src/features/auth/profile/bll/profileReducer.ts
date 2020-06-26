ae6b43dac630e2d3db588924549b0791d8a8fd23
JeremyWylde <63048608+JeremyWylde@users.noreply.github.com>
2020-06-26 17:17:39 +0300

    init commit

---
+++ src/features/auth/profile/bll/profileReducer.ts
@@ -0,0 +1,11 @@
+export const profileReducer  = (state ={}, action: any) =>{
+    switch (action.type) {
+        case " ":
+            return{
+                ...state
+            };
+        default:{
+            return state
+        }
+    }
+};
--- src/main/ui/App.tsx
+++ src/main/ui/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import './App.css';
+import {HashRouter} from "react-router-dom";
+import {Provider} from "react-redux";
+import store from "../bll/Store"
+import Main from "./main/Main";
 
 const App = () => {
     return (
         <div className="App">
-            {/*//hr prov*/}
-            <>
-              {/*<Header/>*/}
-            </>
+            <HashRouter>
+                <Provider store={store}>
+                    <Main/>
+                </Provider>
+            </HashRouter>
         </div>
     );
 };
